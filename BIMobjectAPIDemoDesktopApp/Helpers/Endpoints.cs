0f9a0d218605f183c309c966b53fb0d45b764630
Colin <colin.ingram@bimobject.com>
2017-11-29 09:20:22 +0100

    Update url to use https

--- BIMobjectAPIDemoDesktopApp/Helpers/Endpoints.cs
+++ BIMobjectAPIDemoDesktopApp/Helpers/Endpoints.cs
@@ -4,6 +4,6 @@
     {
         public const string AuthorizationEndpoint = "https://accounts.bimobject.com/identity/connect/authorize";
         public const string TokenEndpoint = "https://accounts.bimobject.com/identity/connect/token";
-        public const string SearchApiBaseUrl = "http://api.bimobject.com/search/v1/products";
+        public const string SearchApiBaseUrl = "https://api.bimobject.com/search/v1/products";
     }
 }
